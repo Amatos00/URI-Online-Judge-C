45d39f2e3f47841e3f98e8ef4440a8493919c275
Anderson de Oliveira <amatos.oliveira2197@gmail.com>
2020-10-10 22:48:37 -0300

    Conversão do tempo

---
+++ 1019/Conversão do tempo/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int tempo, horas, horas_seg, minutos, segundos;
+
+   horas_seg=3600;//horas em segundos
+
+
+   scanf("%d", &tempo);
+   horas = (tempo/horas_seg); //resultado da hora
+   minutos = (tempo -(horas_seg*horas))/60;
+   segundos = (tempo -(horas_seg*horas)-(minutos*60));
+
+   printf("%d:%d:%d\n",horas,minutos,segundos);
+
+
+    return 0;
+}
+
